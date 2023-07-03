180ab8d92db153aedab4b6eaf665ca618590fdbc
amitranjan93 <123475679+amitranjan93@users.noreply.github.com>
2023-07-03 13:53:40 +0530

    Variables of different Data Types

---
+++ VariableExample.java
@@ -0,0 +1,14 @@
+public class VariableExample {
+
+    public static void main(String[] args) {
+        // Declaration and initialization of variables
+        int age = 23;
+        double weight = 72.5;
+        String name = "Amit Ranjan";
+
+        // Printing the variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+    }
+}
